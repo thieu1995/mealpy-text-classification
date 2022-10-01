d63ed8897a229145a6639b4674349d3c39e81c9a
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-10-02 00:16:02 +0700

    Add Logistic Regression Classifier

---
+++ models/classify_lc.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 23:49, 01/10/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+"""
+Logistic Regression (aka logit, MaxEnt) classifier
+
+Logistic regression measures the relationship between the categorical dependent variable and one or
+more independent variables by estimating probabilities using a logistic/sigmoid function.
+"""
+from permetrics.classification import ClassificationMetric
+from sklearn.linear_model import LogisticRegression
+from mealpy.utils.problem import Problem
+
+
+class ClassifyLC(Problem):
+    def __init__(self, lb, ub, minmax, data=None, name="Logistic Regression", **kwargs):
+        super().__init__(lb, ub, minmax, data=data, **kwargs)  ## data is needed because when initialize the Problem class, we need to check the output of fitness
+        self.data = data
+        self.name = name
+
+    def decode_solution(self, solution):
+        # solver_integer = int(solution[1])
+        # solver = SOLVER_ENCODER.inverse_transform([solver_integer])[0]
+        # 0 - 0.99 ==> 0 index ==> should be 'newton-cg' (for example)
+        # 1 - 1.99 ==> 1 index ==> should be 'lbfgs'
+
+        solver_integer = int(solution[1])
+        solver = self.data["SOLVER_ENCODER"].inverse_transform([solver_integer])[0]
+        return {
+            "C": solution[0],
+            "fit_intercept": bool(round(solution[1])),
+            "solver": solver
+        }
+
+    def generate_trained_model(self, structure):
+        # print('Trying to generate trained model...')
+        model = LogisticRegression(C=structure["C"], fit_intercept=structure["fit_intercept"], solver=structure["solver"], max_iter=1000)
+        model.fit(self.data["X_train"], self.data["y_train"])
+        return model
+
+    def generate_loss_value(self, structure):
+        model = self.generate_trained_model(structure)
+
+        # We take the loss value of validation set as a fitness value for selecting the best model demonstrate prediction
+        y_pred = model.predict(self.data["X_valid"])
+        evaluator = ClassificationMetric(self.data["y_valid"], y_pred, decimal=6)
+        acc = evaluator.accuracy_score(average="macro")
+        return 1 - acc
+
+    def fit_func(self, solution):
+        structure = self.decode_solution(solution)
+        fitness = self.generate_loss_value(structure)
+        return fitness
---
+++ run_lc_mealpy.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 00:05, 02/10/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from sklearn.preprocessing import LabelEncoder
+from models.classify_lc import ClassifyLC
+from models.utils.data_util import generate_data
+from models.utils.data_util import features_as_count_vectors, features_as_TF_IDF_vectors
+from permetrics.classification import ClassificationMetric
+from mealpy.swarm_based import WOA
+
+"""
+Tuning hyper-parameter of Logistic Regression model with 3 parameters
+
+C : float, default=1.0
+    Inverse of regularization strength; must be a positive float.
+    Like in support vector machines, smaller values specify stronger regularization.
+
+fit_intercept : bool, default=True
+    Specifies if a constant (a.k.a. bias or intercept) should be added to the decision function.
+
+solver : {'newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'},
+"""
+
+
+if __name__ == "__main__":
+    list_solvers = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+    solver_encoder = LabelEncoder()
+    solver_encoder.fit(list_solvers)
+
+    ## {"train_x": train_x, "y_train": train_y, "valid_x": valid_x, "y_valid": valid_y, "encoder": encoder, "trainDF": trainDF}
+    df = generate_data()
+    df["SOLVER_ENCODER"] = solver_encoder
+
+    ## Tuning hyper-parameter of SVM model with Count Vectors feature engineering
+    X_train, X_valid = features_as_count_vectors(df["trainDF"], df["train_x"], df["valid_x"])
+    df["X_train"] = X_train
+    df["X_valid"] = X_valid
+
+    # x1. C: float [0.1 to 10000.0]
+    # x2. fit_intercept: [0, 1]
+    # x3. solver: [‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’]
+    LB = [0.1, 0., 0.]
+    UB = [10000.0, 1.0, 3.99]
+
+    problem = ClassifyLC(lb=LB, ub=UB, minmax="min", data=df, save_population=False, log_to="console")
+
+    algorithm = WOA.OriginalWOA(epoch=10, pop_size=20)
+    best_position, best_fitness = algorithm.solve(problem)
+
+    best_solution = problem.decode_solution(best_position)
+
+    print(f"Best fitness (accuracy score) value: {1 - best_fitness}")
+    print(f"Best parameters: {best_solution}")
+
+    ###### Get the best tuned model to predict test set
+    best_model = problem.generate_trained_model(best_solution)
+    y_valid = best_model.predict(df["X_valid"])
+
+    evaluator = ClassificationMetric(df["y_valid"], y_valid, decimal=6)
+    print(evaluator.get_metrics_by_list_names(["AS", "RS", "PS", "F1S", "F2S"]))
